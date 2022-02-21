a6e30270d15ebb7d5c8a54bad2fc96743a54f58d
Sebastian Itokazu <sebitokazu@gmail.com>
2022-02-21 17:23:21 -0300

    Mint domain

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import "./styles/App.css";
 import twitterLogo from "./assets/twitter-logo.svg";
+import { ethers } from "ethers";
+import contractAbi from "./utils/contractABI.json";
 
 // Constants
 const BUILDSPACE_TWITTER_HANDLE = "_buildspace";
@@ -8,6 +10,7 @@ const PROFILE_TWITTER_HANDLE = "seba_itokazu";
 const BUILDSPACE_TWITTER_LINK = `https://twitter.com/${BUILDSPACE_TWITTER_HANDLE}`;
 const PROFILE_TWITTER_LINK = `https://twitter.com/${PROFILE_TWITTER_HANDLE}`;
 const tld = ".ibis";
+const CONTRACT_ADDRESS = "0x093B1aD72470F4388DEA7F39137884b4f0be6226";
 
 const App = () => {
   const [currentAccount, setCurrentAccount] = useState("");
@@ -50,8 +53,6 @@ const App = () => {
     // Check if we're authorized to access the user's wallet
     const accounts = await ethereum.request({ method: "eth_accounts" });
 
-    const currentAccount = await ethereum.request({ method: "" });
-
     // Users can have multiple authorized accounts, we grab the first one if its there!
     if (accounts.length !== 0) {
       const account = accounts[0];
@@ -59,6 +60,66 @@ const App = () => {
       setCurrentAccount(account);
     } else {
       console.log("No authorized account found");
+    }
+  };
+
+  const mintDomain = async () => {
+    // Don't run if the domain is empty
+    if (!domain) {
+      return;
+    }
+    // Calculate price based on length of domain
+    // 3 or less chars = 0.5 MATIC, 4 chars = 0.3 MATIC, 5 or more = 0.1 MATIC
+    const price =
+      domain.length <= 3 ? "0.5" : domain.length === 4 ? "0.3" : "0.1";
+    console.log("Minting domain", domain, "with price", price);
+    try {
+      const { ethereum } = window;
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const contract = new ethers.Contract(
+          CONTRACT_ADDRESS,
+          contractAbi.abi,
+          signer
+        );
+
+        console.log("Going to pop wallet now to pay gas...");
+        let tx = await contract.register(domain, {
+          value: ethers.utils.parseEther(price),
+        });
+        // Wait for the transaction to be mined
+        const receipt = await tx.wait();
+
+        // Check if the transaction was successfully completed
+        if (receipt.status === 1) {
+          console.log(
+            "Domain minted! https://mumbai.polygonscan.com/tx/" + tx.hash
+          );
+
+          // Set the record for the domain
+          tx = await contract.setAllRecords(
+            domain,
+            nickname,
+            spotifyLink,
+            twitter
+          );
+          await tx.wait();
+
+          console.log(
+            "All records set! https://mumbai.polygonscan.com/tx/" + tx.hash
+          );
+
+          setNickname("");
+          setSpotifyLink("");
+          setTwitter("");
+          setDomain("");
+        } else {
+          alert("Transaction failed! Please try again");
+        }
+      }
+    } catch (error) {
+      console.log(error);
     }
   };
 
@@ -98,11 +159,25 @@ const App = () => {
           onChange={(e) => setNickname(e.target.value)}
         />
 
+        <input
+          type="text"
+          value={spotifyLink}
+          placeholder="Your Spotify fav song"
+          onChange={(e) => setSpotifyLink(e.target.value)}
+        />
+
+        <input
+          type="text"
+          value={twitter}
+          placeholder="Your twitter account"
+          onChange={(e) => setTwitter(e.target.value)}
+        />
+
         <div className="button-container">
           <button
             className="cta-button mint-button"
             disabled={null}
-            onClick={null}
+            onClick={mintDomain}
           >
             Mint
           </button>
