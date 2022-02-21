7e86285e14026d9ff95284b87940e7b6bb50f133
Sebastian Itokazu <sebitokazu@gmail.com>
2022-02-21 16:10:49 -0300

    Connect wallet

--- src/App.js
+++ src/App.js
@@ -1,48 +1,162 @@
-import React from 'react';
-import './styles/App.css';
-import twitterLogo from './assets/twitter-logo.svg';
+import React, { useEffect, useState } from "react";
+import "./styles/App.css";
+import twitterLogo from "./assets/twitter-logo.svg";
 
 // Constants
-const BUILDSPACE_TWITTER_HANDLE = '_buildspace';
-const PROFILE_TWITTER_HANDLE = 'seba_itokazu';
+const BUILDSPACE_TWITTER_HANDLE = "_buildspace";
+const PROFILE_TWITTER_HANDLE = "seba_itokazu";
 const BUILDSPACE_TWITTER_LINK = `https://twitter.com/${BUILDSPACE_TWITTER_HANDLE}`;
 const PROFILE_TWITTER_LINK = `https://twitter.com/${PROFILE_TWITTER_HANDLE}`;
+const tld = ".ibis";
 
 const App = () => {
+  const [currentAccount, setCurrentAccount] = useState("");
+  const [domain, setDomain] = useState("");
+  const [nickname, setNickname] = useState("");
+  const [spotifyLink, setSpotifyLink] = useState("");
+  const [twitter, setTwitter] = useState("");
+
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert("Get MetaMask -> https://metamask.io/");
+        return;
+      }
+
+      // Fancy method to request access to account.
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+
+      console.log("Connected", accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const checkIfWalletIsConnected = async () => {
+    const { ethereum } = window;
+
+    if (!ethereum) {
+      console.log("Make sure you have MetaMask!");
+      return;
+    } else {
+      console.log("We have the ethereum object", ethereum);
+    }
+
+    // Check if we're authorized to access the user's wallet
+    const accounts = await ethereum.request({ method: "eth_accounts" });
+
+    const currentAccount = await ethereum.request({ method: "" });
+
+    // Users can have multiple authorized accounts, we grab the first one if its there!
+    if (accounts.length !== 0) {
+      const account = accounts[0];
+      console.log("Found an authorized account:", account);
+      setCurrentAccount(account);
+    } else {
+      console.log("No authorized account found");
+    }
+  };
+
+  const renderNotConnectedContainer = () => (
+    <div className="connect-wallet-container">
+      {/* <img
+        src="https://media.giphy.com/media/3ohhwytHcusSCXXOUg/giphy.gif"
+        alt="Ninja gif"
+      /> */}
+      <button
+        className="cta-button connect-wallet-button"
+        onClick={connectWallet}
+      >
+        Connect Wallet
+      </button>
+    </div>
+  );
+
+  // Form to enter domain name and data
+  const renderInputForm = () => {
+    return (
+      <div className="form-container">
+        <div className="first-row">
+          <input
+            type="text"
+            value={domain}
+            placeholder="domain"
+            onChange={(e) => setDomain(e.target.value)}
+          />
+          <p className="tld"> {tld} </p>
+        </div>
+
+        <input
+          type="text"
+          value={nickname}
+          placeholder="Your nickname"
+          onChange={(e) => setNickname(e.target.value)}
+        />
+
+        <div className="button-container">
+          <button
+            className="cta-button mint-button"
+            disabled={null}
+            onClick={null}
+          >
+            Mint
+          </button>
+          <button
+            className="cta-button mint-button"
+            disabled={null}
+            onClick={null}
+          >
+            Set data
+          </button>
+        </div>
+      </div>
+    );
+  };
+
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, []);
 
   return (
-		<div className="App">
-			<div className="container">
-
-				<div className="header-container">
-					<header>
+    <div className="App">
+      <div className="container">
+        <div className="header-container">
+          <header>
             <div className="left">
               <p className="title">🚀 Ibis Name Service</p>
               <p className="subtitle">Your immortal API on the blockchain!</p>
             </div>
-					</header>
-				</div>
+          </header>
+        </div>
+
+        {!currentAccount && renderNotConnectedContainer()}
+        {currentAccount && renderInputForm()}
 
         <div className="footer-container">
-					<img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
-					built with &nbsp;
-					<a
-						className="footer-text"
-						href={BUILDSPACE_TWITTER_LINK}
-						target="_blank"
-						rel="noreferrer"
-					>{`@${BUILDSPACE_TWITTER_HANDLE}`}</a>
-					&nbsp; by &nbsp;
-					<a
-						className="footer-text"
-						href={PROFILE_TWITTER_LINK}
-						target="_blank"
-						rel="noreferrer"
-					>{`@${PROFILE_TWITTER_HANDLE}`}</a>
-				</div>
-			</div>
-		</div>
-	);
-}
+          <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
+          built with &nbsp;
+          <a
+            className="footer-text"
+            href={BUILDSPACE_TWITTER_LINK}
+            target="_blank"
+            rel="noreferrer"
+          >{`@${BUILDSPACE_TWITTER_HANDLE}`}</a>
+          &nbsp; by &nbsp;
+          <a
+            className="footer-text"
+            href={PROFILE_TWITTER_LINK}
+            target="_blank"
+            rel="noreferrer"
+          >{`@${PROFILE_TWITTER_HANDLE}`}</a>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default App;
